1774b1affc2af8edb642d93f46c8a3ca4b3f9b7a
zi-won <aktos109@gmail.com>
2025-04-10 18:00:29 +0900

    json 서버 설정 방법까지 완료

--- 15_vue/06_slot/src/App.vue
+++ 15_vue/06_slot/src/App.vue
@@ -1,25 +1,36 @@
 <!-- Vue Official/Vue Vscode Snippet 플러그인 설치 -->
 <!-- vbase-3-setup으로 틀을 잡고 scss관련 부분 삭제 -->
-
- <!--
+ 
+<!--
     template
-    vue는 사용자의 화면에 보여질 부분을 template 영역에 html 요소들로 작성한다.
+    vue는 사용자의 화면에 보여질 부분을 template 영역에 html 요소들로 작성한다. 
     (template 태그는 DOM tree로 구성되지 않고 렌더링 될 때 해석되지 않는다.)
- -->
+-->
 <template>
-    <div class="redColor">
-        <p>잘 나오나?</p>
-        <MyComponent/>
-    </div>
+    <header>
+        <div class="redColor">
+            <p>잘 나오나?</p>
+            <MyComponent/>
+        </div>
+    </header>
+    <main>
+        <SlotParent/>
+        <NamedParent/>
+        <DynamicParent/>
+    </main>
 </template>
 
 <!--
     script setup
-    해당 컴포넌트에 적용할 setup 안에 작성했던 내용(반응형 변수/ 반응형 변수를 활용하는 함수/라이프사이클 훅)이 작성되는 영역
+    해당 컴포넌트에 적용할 setup 안에 작성했던 내용(반응형 변수/반응형 변수를 활용하는 함수/라이프사이클 훅)이
+    작성되는 영역
 -->
 <script setup>
     // import MyComponent from './components/01_component/MyComponent.vue';
     import MyComponent from '@/components/01_component/MyComponent.vue';
+    import SlotParent from './components/02_slotintro/SlotParent.vue';
+    import NamedParent from './components/03_namedslot/NamedParent.vue';
+    import DynamicParent from './components/04_dynamicslot/DynamicParent.vue';
 </script>
 
 <!--
--- 15_vue/06_slot/src/components/01_component/MyComponent.vue
+++ 15_vue/06_slot/src/components/01_component/MyComponent.vue
@@ -5,7 +5,17 @@
 </template>
 
 <script setup>
+    import { onMounted } from 'vue';
 
+        // export default{
+        //     setup() {
+             onMounted(() => {
+                console.log('MyComponent 부착!');
+                let $idEle = document.getElementById('test');
+                console.log($idEle);
+             })
+        //     }
+        // }
 </script>
 
 <style scoped>
